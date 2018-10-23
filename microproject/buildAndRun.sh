13eea255787de9ce8d3718bff3b7fe098406904f
David Stoettinger <david.stoettinger@tele2.at>
2018-10-23 11:42:36 +0200

    Created Project and added JPA to it ( with Docker)

--- microproject/buildAndRun.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-mvn clean package && docker build -t at.htl.nvs/microproject .
-docker rm -f microproject || true && docker run -d -p 8080:8080 -p 4848:4848 --name microproject at.htl.nvs/microproject
---
+++ microproject_demo/demo/src/main/java/at/htl/nvs/demo/rest/RestApplication.java
@@ -0,0 +1,9 @@
+package at.htl.nvs.demo.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("rs")
+public class RestApplication extends Application {
+
+}
